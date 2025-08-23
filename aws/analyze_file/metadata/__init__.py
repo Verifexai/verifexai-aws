61625a25de4f1d4138d7496e3859cb8e67c9f717
Yair Hadad <Yair_hadad@epam.com>
2025-08-23 15:09:01 +0300

    remove code from testing

--- aws/analyze_file/metadata/__init__.py
+++ aws/analyze_file/metadata/__init__.py
@@ -7,15 +7,9 @@ from aws.common.utilities.enums import FileType
 def analyze_metadata_check(local_file_path: str,
                     file_type: FileType,
                     label_data: Dict[str, Any]=None):
-    from metadata_checks import analyze_metadata as _analyze_metadata
+    from .metadata_checks import analyze_metadata as _analyze_metadata
 
     return _analyze_metadata(local_file_path=local_file_path,file_type=file_type,label_data=label_data)
 
 
 __all__ = ["analyze_metadata_check"]
-
-if __name__ == '__main__':
-    print("start")
-    file_path = "יואב אליעזר טרנטר מוזר - טופס אישור העסקה.pdf"
-    result = analyze_metadata_check(file_path,file_type=FileType.TaxCertificate)
-    print(result)
